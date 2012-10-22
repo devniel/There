e0b22566f0733a9b3d1a7d70dc6e9d320fd0b0a6
unknown <miguel@ITLAB-V5713.itlab.com>
2012-10-22 12:16:03 -0500

    interfaces registro(terminado) main(arreglando geolocalizar(empezando))

--- src/controllers/MainActivity.java
+++ src/controllers/MainActivity.java
@@ -6,10 +6,16 @@ import com.softudy.there.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
+	Button button;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,4 +27,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void addListenerOnButton() {
+    	 
+		final Context context = this;
+ 
+		button = (Button) findViewById(R.id.buttonPrincRegistrarse);
+ 
+		button.setOnClickListener(new OnClickListener() {
+			
+			
+			
+			public void onClick(View arg0) {
+				System.out.println("aca");
+			    Intent intent = new Intent(context, RegistroMain.class);
+                            startActivity(intent);   
+                System.out.println("llego");
+			}
+ 
+		});
+    
+   
 }
+    }
